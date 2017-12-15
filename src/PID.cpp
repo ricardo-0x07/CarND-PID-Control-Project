98736d153a415f760419aea377d2046f57a6137f
Clive R. Cadogan <ricardo_0x07@yahoo.com>
2017-12-15 16:11:53 -0400

    added comments to PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -20,11 +20,13 @@ void PID::Init(double Kp, double Ki, double Kd) {
 }
 
 void PID::UpdateError(double cte) {
+    // Compute previous cte from previous proportional error term and and hyperparameter (p_error = Kp * cte;)   
     double pre_cte = p_error/Kp;
+    // Compute accumulated cte by first computing the previous accumulated cte from integral error term and and hyperparameter (i_error = Ki * acc_cte;)and the adding the new cte  
     double acc_cte = (i_error/Ki) + cte;
     this->p_error = this->Kp * cte;
     this->i_error = this->Ki * acc_cte;
-    this->d_error = this->Kd * (cte -pre_cte);
+    this->d_error = this->Kd * (cte - pre_cte);
 }
 
 double PID::TotalError() {
